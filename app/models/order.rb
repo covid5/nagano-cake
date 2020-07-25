5dcfb64df5a4883773528dcf1620b8f1793421ac
nogamieri <komeuma@gmail.com>
2020-07-25 12:06:10 +0000

    コンフリクト修正もれ

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,10 @@
 class Order < ApplicationRecord
-<<<<<<< HEAD
   enum payment_method:{credit_card: 0, bank_transfer: 1}
-
-=======
 
 	belongs_to :member
 	has_many :order_detail
 
 attachments :profile_image
->>>>>>> develop
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_07_25_060211) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.boolean "disabled", default: false, null: false
-=======
+
 ActiveRecord::Schema.define(version: 2020_07_19_075338) do
 
 
@@ -35,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_052636) do
     t.string "phone_number"
     t.string "encrypted_password"
     t.boolean "status"
->>>>>>> develop
+
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -48,7 +47,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_052636) do
     t.integer "total_amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-=======
+
   create_table "order_details", force: :cascade do |t|
     t.integer "product_id"
     t.integer "order_id"
@@ -61,13 +60,48 @@ ActiveRecord::Schema.define(version: 2020_07_19_052636) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "member_id"
->>>>>>> develop
+
+ActiveRecord::Schema.define(version: 2020_07_19_075338) do
+
+
+
+
+ActiveRecord::Schema.define(version: 2020_07_19_052636) do
+
+  create_table "members", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "first_name_kana"
+    t.string "last_name_kana"
+    t.string "email"
+    t.string "postal_code"
+    t.string "address"
+    t.string "phone_number"
+    t.string "encrypted_password"
+    t.boolean "status"
+
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+<<<<<<< HEAD
+  create_table "order_details", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.integer "number"
+    t.integer "taxed_price"
+    t.integer "production_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "member_id"
+
     t.string "postal_code"
     t.string "address"
     t.string "address_name"
     t.integer "status"
-<<<<<<< HEAD
-=======
     t.integer "postage"
     t.integer "payment_method"
     t.integer "total_amount"
@@ -119,7 +153,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_075338) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
->>>>>>> develop
+
   end
 
   create_table "products", force: :cascade do |t|
@@ -128,8 +162,6 @@ ActiveRecord::Schema.define(version: 2020_07_19_075338) do
     t.integer "price"
     t.string "image_id"
     t.integer "salling_status"
-<<<<<<< HEAD
-=======
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
 
@@ -138,7 +170,6 @@ ActiveRecord::Schema.define(version: 2020_07_19_075338) do
     t.string "name"
     t.boolean "disabled", default: false, null: false
 
->>>>>>> develop
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
