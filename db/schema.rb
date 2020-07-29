5c9bc7997c4929c239832b5d1258fc29a435a0f3
nogamieri <komeuma@gmail.com>
2020-07-29 15:10:55 +0000

    フォーム途中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_075338) do
+ActiveRecord::Schema.define(version: 2020_07_19_055113) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -80,9 +80,9 @@ ActiveRecord::Schema.define(version: 2020_07_19_075338) do
     t.integer "price"
     t.string "image_id"
     t.integer "salling_status"
+    t.integer "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "genre_id"
   end
 
   create_table "shipping_addresses", force: :cascade do |t|
